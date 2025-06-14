ee7b87d3d3b809bd3c25e351a01117fb8b44aa35
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2025-06-14 20:40:00 +0700

    Update InstallStep.vue

--- src/components/InstallStep.vue
+++ src/components/InstallStep.vue
@@ -5,7 +5,7 @@
 
             <div class="text-body-1">
                 <p>
-                    This will install {{ $root.$data.OS_NAME }}
+                    This will install
                     {{ $root.$data.release.version
                     }}{{
                         $root.$data.RELEASE_VARIANTS[
